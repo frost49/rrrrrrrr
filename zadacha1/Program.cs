43d712a91a194b647e9d0c5cfcdb8fc666320c19
frost49 <frost49@mail.ru>
2022-12-28 21:11:59 +0400

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b*b==a)
+ Console.WriteLine("Первое число явлется квадратом второго! ");
+
+
+else
+Console.WriteLine("Первое число не является квадратом второго ");
