365a3b95b1e574281554a9fc0b088941de154bb1
frost49 <frost49@mail.ru>
2022-12-29 19:19:04 +0400

    zadacha10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int b = a / 10 % 10; 
+if (a > 99 && a <1000) 
+{
+string str = $"Второе = {b} ";
+Console.Write(str);
+}
+else Console.Write("Это не трехзначное число");
