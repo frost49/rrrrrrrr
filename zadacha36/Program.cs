5e2ec3df3c79a39c11a08856f187959deb826d22
frost49 <frost49@mail.ru>
2022-12-29 20:33:18 +0400

    zadacha36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[5];
+int sum = 0;
+for (int i =0; i <array.Length; i++)
+{
+    array [i] = new Random().Next(1, 5);
+    Console.Write(array[i] + ", ");
+}
+for (int i =0; i <array.Length; i++)
+{
+    if (i % 2==1)
+    sum = sum + array[i];
+}
+Console.Write($"Сумма элементов, стоящих на нечетных позициях -> {sum}");
