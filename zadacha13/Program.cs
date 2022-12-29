5cc15fd80587c2fdb759bcfc29fb6dd9d5121307
frost49 <frost49@mail.ru>
2022-12-29 19:27:38 +0400

    zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+string numStr = $"{number}";
+int ln = numStr.Length;
+
+if (ln >= 3 )
+{
+    Console.Write("Третье число = ");
+    Console.Write(numStr[2]);
+}
+else
+    Console.Write("третьей цифры нет");
