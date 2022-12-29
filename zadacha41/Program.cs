0e1a263679da89cb465418186efa76710edcc8bf
frost49 <frost49@mail.ru>
2022-12-29 20:47:33 +0400

    zadacha41

---
+++ zadacha41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите цифры через пробел");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i <arr.Length; i++)
+{
+    if(arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество цифр больше нуля: {count} ");
