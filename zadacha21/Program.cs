10913454618765ed71d2f4fd25eb429ad354d6b0
frost49 <frost49@mail.ru>
2022-12-29 20:02:15 +0400

    zadacha21

---
+++ zadacha21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+double z = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите четвертое число: ");
+double a1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите пятое число: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите шестое число: ");
+double z1 = double.Parse(Console.ReadLine()!);
+
+
+double ab = Math.Sqrt(Math.Pow(a-a1, 2) + Math.Pow(b - b1, 2)+Math.Pow(z-z1, 2));
+
+Console.WriteLine(ab);
