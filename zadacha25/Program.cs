dbf45b6e40b30a5cb6fcdb6a9490d39fa9fb67a2
frost49 <frost49@mail.ru>
2022-12-29 20:13:57 +0400

    zadacha25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+int c = a;
+int i = 1;
+while (i < b)
+{
+    c = c * a;
+    i++;
+}
+Console.Write("A в степени B равно: " + c);
