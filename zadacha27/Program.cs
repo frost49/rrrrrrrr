ad6606952e3e3d25b9de1dd736bb9893ed460621
frost49 <frost49@mail.ru>
2022-12-29 20:17:52 +0400

    zadacha27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число -> ");
+int x = int.Parse(Console.ReadLine()!);
+string string_x = Convert.ToString(x);
+
+int summ = 0;
+
+for (int i = 0; i < string_x.Length; i++)
+{
+   string a = Convert.ToString(string_x[i]);
+   summ = summ + Convert.ToInt32(a);
+}
+Console.Write(summ);
+
