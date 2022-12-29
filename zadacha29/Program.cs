b655745436e74b0340f497f192e14cac3480d0c9
frost49 <frost49@mail.ru>
2022-12-29 20:23:22 +0400

    zadacha29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,11 @@
+﻿int[]  array = new int[8];
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    string elements = Convert.ToString(array[i]);
+    int convert = Convert.ToInt32(elements);
+    Console.Write(convert.ToString() + ", ");
+}
+Console.Write("]");
