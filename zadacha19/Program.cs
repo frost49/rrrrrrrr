9a4ae0f08bc5c798e294f9bc95fc48492b359138
frost49 <frost49@mail.ru>
2022-12-29 19:57:54 +0400

    zadacha19

---
+++ zadacha19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+int b = number.Length;
+
+if (b == 5 )
+
+if ( number[0] == number[4] && number[1] == number[3])
+Console.WriteLine($"{number} - Палиндром");
+
+else
+Console.Write("не палиндром.");
+
+else 
+Console.Write("не пятизначное число");
