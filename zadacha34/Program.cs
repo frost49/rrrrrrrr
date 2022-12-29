45b61c5f5b16e693d3a826890b0ef13634e26ad0
frost49 <frost49@mail.ru>
2022-12-29 20:29:55 +0400

    zadacha34

---
+++ zadacha34/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[10];
+int count = 0;
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (i == array.Length - 1)
+    {
+        Console.Write(array[i] + "]");
+    }
+    else 
+    {
+        Console.Write(array[i] + ", ");
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество чётных чисел в массиве: " + count);
