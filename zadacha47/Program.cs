d47d78eb90f62de79e98c0bebb32c63b38d2ad4d
frost49 <frost49@mail.ru>
2023-01-05 21:52:46 +0400

    Zadacha47

---
+++ zadacha47/Program.cs
@@ -0,0 +1,13 @@
+﻿double[,] array = new double[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double number = new Random().Next(-10,10) + new Random().NextDouble();
+        number = Math.Round (number, 1);
+        array [i,j] = number;
+         Console.Write(array[i,j] + ", ");
+    }
+    Console.WriteLine();
+}
